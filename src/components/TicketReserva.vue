f8adfbabf974dba162d5146ed17b746b37224e84
nikyuher <totalgamenicogmail.com>
2024-02-25 20:21:17 +0100

    usuario no puede modificar datos ticker

--- src/components/TicketReserva.vue
+++ src/components/TicketReserva.vue
@@ -5,7 +5,7 @@ import { defineProps } from 'vue';
 const props = defineProps<{
     titulo: string;
     precio: number;
-    butaca:string;
+    butaca: string;
 }>();
 
 const handleCompra = () => {
@@ -18,12 +18,14 @@ const handleCompra = () => {
     <h2>Reserva Asiento</h2>
     <div class="contenidoForm">
         <form @submit.prevent="handleCompra">
-            <label>Obra de Teatro</label>
-            <input type="text" id="inputNombreObra" :value="titulo">
-            <label>Sitio de Asiento</label>
-            <input type="text" id="inputSitioAsiento" required :value="butaca">
-            <label>Precio:$</label>
-            <input type="number" id="inputPrecio" :value="precio">
+            <div class="datosForm">
+                <label>Obra de Teatro</label>
+                <input type="text" id="inputNombreObra" :value="titulo">
+                <label>Sitio de Asiento</label>
+                <input type="text" id="inputSitioAsiento" required :value="butaca">
+                <label>Precio:$</label>
+                <input type="number" id="inputPrecio" :value="precio">
+            </div>
             <input id="comprar" type="submit" value="Comprar">
         </form>
     </div>
@@ -35,7 +37,11 @@ const handleCompra = () => {
     width: 200px;
     margin: auto;
 }
+
 .contenidoForm #comprar {
     margin-top: 20px;
+}
+.datosForm{
+    pointer-events: none;
 }
 </style>
